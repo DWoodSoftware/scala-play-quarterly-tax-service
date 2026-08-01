f8ad229e1874369760194ef2677e493ccc1160c9
DavidWood2001 <davidwood1301@gmail.com>
2026-08-01 19:19:00 +0100

    tests: define validation error format invariants

---
+++ test/domain/ValidationErrorSpec.scala
@@ -0,0 +1,16 @@
+package domain
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class ValidationErrorSpec extends AnyWordSpec with Matchers {
+    
+    "ValidationError" should {
+
+        "expose only recognised domain validation failures" in {
+            ValidationError.values.toSet shouldBe Set(
+                ValidationError.MissingIncome
+            )
+        }
+    }
+}
