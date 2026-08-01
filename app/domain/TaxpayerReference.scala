dea70300e93b6ec01c73141a552c0b2ade179fe8
DavidWood2001 <davidwood1301@gmail.com>
2026-08-01 19:10:24 +0100

    feat: implement validated taxpayer reference type

---
+++ app/domain/TaxpayerReference.scala
@@ -0,0 +1,14 @@
+package domain
+
+final case class TaxpayerReference private (value: String)
+
+object TaxpayerReference {
+  private val Pattern = "^TAX-\\d{8}$".r
+
+  def create(value: String): Either[String, TaxpayerReference] = {
+    value match {
+      case Pattern() => Right(TaxpayerReference(value))
+      case _ => Left("Invalid taxpayer reference format. Expected format: TAX-XXXXXXXX where X is a digit.")
+    }
+  }
+}
