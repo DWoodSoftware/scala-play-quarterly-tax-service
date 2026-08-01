4612489bcbdac04197ee88ecc86d57a61af66d03
DavidWood2001 <davidwood1301@gmail.com>
2026-08-01 17:28:53 +0100

    feat: implement typed financial categories

---
+++ app/domain/ExpenseCategory.scala
@@ -0,0 +1,10 @@
+package domain
+
+enum ExpenseCategory {
+    case Travel 
+    case OfficeCosts 
+    case ProfessionalFees 
+    case Advertising 
+    case Equipment 
+    case Other
+}
---
+++ app/domain/IncomeCategory.scala
@@ -0,0 +1,11 @@
+package domain
+
+enum IncomeCategory {
+    case SelfEmployment 
+    case Property 
+    case Investment 
+    case Pension 
+    case StateBenefits 
+    case Dividends 
+    case Other
+}
