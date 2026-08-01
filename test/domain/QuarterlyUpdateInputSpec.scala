781381c511ec79d8dd7cabe1a0c496305dc53a31
DavidWood2001 <davidwood1301@gmail.com>
2026-08-01 18:41:53 +0100

    test: define quarterly update input contract

---
+++ test/domain/QuarterlyUpdateInputSpec.scala
@@ -0,0 +1,105 @@
+package domain
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class QuarterlyUpdateInputSpec extends AnyWordSpec with Matchers {
+
+    "QuarterlyUpdateInput" should {
+
+        "carry only trusted client-supplied quarterly reporting data" in {
+            val taxYear = 
+                TaxYear.create(2026, 2027) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val incomeEntry =
+                IncomeEntry.create(
+                    IncomeCategory.SelfEmployment, 
+                    BigDecimal("1500.00")
+                ) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val expenseEntry =
+                ExpenseEntry.create(
+                    ExpenseCategory.OfficeCosts, 
+                    BigDecimal("250.00")
+                ) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val input = QuarterlyUpdateInput(
+                taxPayerReference = "TEST-12345",
+                taxYear = taxYear,
+                quarter = Quarter.Q1,
+                income = List(incomeEntry),
+                expenses = List(expenseEntry)
+            )
+
+            input.taxPayerReference shouldEqual "TEST-12345"
+            input.taxYear shouldEqual taxYear
+            input.quarter shouldEqual Quarter.Q1
+            input.income shouldEqual List(incomeEntry)
+            input.expenses shouldEqual List(expenseEntry)
+        }
+
+        "allow multiple income and expense entries" in {
+            val taxYear = 
+                TaxYear.create(2026, 2027) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val selfEmployment =
+                IncomeEntry.create(
+                    IncomeCategory.SelfEmployment, 
+                    BigDecimal("1500.00")
+                ) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val dividends =
+                IncomeEntry.create(
+                    IncomeCategory.Dividends, 
+                    BigDecimal("200.00")
+                ) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val officeCosts =
+                ExpenseEntry.create(
+                    ExpenseCategory.OfficeCosts, 
+                    BigDecimal("250.00")
+                ) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val travel =
+                ExpenseEntry.create(
+                    ExpenseCategory.Travel, 
+                    BigDecimal("80.00")
+                ) match {
+                    case Right(value) => value
+                    case Left(error) => fail(error)
+                }
+            
+            val input = QuarterlyUpdateInput(
+                taxPayerReference = "TEST-12345",
+                taxYear = taxYear,
+                quarter = Quarter.Q1,
+                income = List(selfEmployment, dividends),
+                expenses = List(officeCosts, travel)
+            )
+
+            input.income should contain theSameElementsAs List(selfEmployment, dividends)
+            input.expenses should contain theSameElementsAs List(officeCosts, travel)
+        }
+    }
+}
