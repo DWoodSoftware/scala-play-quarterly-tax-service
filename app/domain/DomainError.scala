a8f15d78f5590e7fa6265c489ddf629410cbaeeb
DavidWood2001 <davidwood1301@gmail.com>
2026-08-03 00:36:34 +0100

    feat: implement application domain error model

---
+++ app/domain/DomainError.scala
@@ -0,0 +1,15 @@
+package domain
+
+enum DomainError:
+    case ValidationFailed(
+        errors: List[ValidationError]
+    )
+
+    case UpdateNotFound(
+        id: String
+    )
+
+    case InvalidStateTransition(
+        current: SubmissionStatus,
+        requested: SubmissionStatus
+    )
