6ed17c865fd4c75db07cba60937c24d163c1de96
DavidWood2001 <davidwood1301@gmail.com>
2026-08-03 00:10:06 +0100

    test: define in-memory repository save and lookup behaviour

---
+++ test/repositories/InMemoryQuarterlyUpdateRepositorySpec.scala
@@ -0,0 +1,56 @@
+
+package repositories
+
+import domain.* 
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+
+class InMemoryQuarterlyUpdateRepositorySpec extends AsyncWordSpec with Matchers {
+
+  "InMemoryQuarterlyUpdateRepository" should {
+
+    "save and retrieve a quarterly update by id" in {
+        val repository: QuarterlyUpdateRepository =
+            new InMemoryQuarterlyUpdateRepository
+
+        val update = QuarterlyUpdate.create(validInput())
+
+        repository.save(update).flatMap { saved =>
+            repository.findById(saved.id).map { result =>
+                result shouldBe Some(saved)
+            }
+        }
+    }
+  }
+
+  private def validInput(): QuarterlyUpdateInput = {
+    val taxpayerReference =
+        TaxpayerReference.create("TAX-12345678") match {
+            case Right(value) => value
+            case Left(error) => fail(error)
+        }
+
+    val taxYear =
+        TaxYear.create(2026, 2027) match {
+            case Right(value) => value
+            case Left(error) => fail(error)
+        }
+
+    val income =
+        IncomeEntry.create(
+            IncomeCategory.SelfEmployment,
+            BigDecimal("1500.00")
+        ) match {
+            case Right(value) => value
+            case Left(error) => fail(error)
+        }
+    
+    QuarterlyUpdateInput(
+        taxpayerReference = taxpayerReference,
+        taxYear = taxYear,
+        quarter = Quarter.Q1,
+        income = List(income),
+        expenses = List.empty
+    )
+  }
+}
